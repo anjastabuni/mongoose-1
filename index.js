4e2574de282841012c355973cab2eff81d130257
username_anjastabuni <reblytabuni2004@gmail.com>
2025-01-24 17:46:51 +0700

    koneksikan mongoose ke mongodb dan buat schema data

---
+++ index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/movie_db")
+  .then((result) => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  director: String,
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+const movie = new Movie({
+  title: "Black Panther",
+  year: 2025,
+  score: 8.3,
+  director: "Ryan Coogler",
+});
+
+movie.save();
+console.log(movie);
