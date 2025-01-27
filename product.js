d829aaf62fe3208f63df823fe90390141d3f14bc
username_anjastabuni <reblytabuni2004@gmail.com>
2025-01-27 14:47:42 +0700

    membuat schema

--- product.js
+++ product.js
@@ -1,9 +1,73 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/movie_db")
+  .connect("mongodb://127.0.0.1:27017/ShopApp")
   .then((result) => {
     console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  size: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  description: {
+    type: String,
+    required: true,
+    maxLength: 150,
+  },
+  condition: {
+    type: String,
+    enum: ["baru", "bekas"],
+    default: "baru",
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  availability: {
+    online: {
+      type: Boolean,
+      required: true,
+    },
+    offline: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+const tshirt = new Product({ name: "T Shirt", price: 500000, color: "Black" });
+
+tshirt
+  .save()
+  .then((result) => {
+    console.log(result);
   })
   .catch((err) => {
     console.log(err);
